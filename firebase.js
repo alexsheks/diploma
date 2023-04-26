4d8d5f22b9c4fb203d5372eff0ce69f84249dbe8
alexsheks <73126312+alexsheks@users.noreply.github.com>
2023-04-26 19:54:25 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyANRLseBV8r2kBYX7Hkx-4HFb_YEFOZpC0",
-  authDomain: "just-an-atom.firebaseapp.com",
-  projectId: "just-an-atom",
-  storageBucket: "just-an-atom.appspot.com",
-  messagingSenderId: "893836291939",
-  appId: "1:893836291939:web:6e0214c54b60d7add5ad56",
-  measurementId: "G-4ZL9YQY6HK",
-};
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+}
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
